6ddd2fb4794ea77a3fb1b62c3a27276d39485da4
hafizharoon1521 <developerhafizmuhammadharoon@gmail.com>
2026-04-21 16:41:52 +0500

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/card-system/"
 })
-export default {
-  base: "/card-system/",
-};
